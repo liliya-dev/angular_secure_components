76060af48b5ad92d18c8d136219dc2295740eb32
liliya-dev <liliya.c@halo-lab.team>
2020-11-03 12:35:15 +0200

    card row

--- src/app/molecules/card-row/card-row.component.ts
+++ src/app/molecules/card-row/card-row.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-card-row',
   templateUrl: './card-row.component.html',
-  styleUrls: ['./card-row.component.scss']
+  styleUrls: ['./card-row.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
+
 export class CardRowComponent implements OnInit {
 
   constructor() { }
