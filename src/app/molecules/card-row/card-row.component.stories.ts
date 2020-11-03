db28701ca0c66be4a2ab634d80d4bde5dcea1aff
lili4ka <lili4ka@MacBook-Pro-lili4ka.local>
2020-11-03 13:47:34 +0200

    adaptive card row

--- src/app/molecules/card-row/card-row.component.stories.ts
+++ src/app/molecules/card-row/card-row.component.stories.ts
@@ -1,6 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Meta, moduleMetadata, Story } from '@storybook/angular';
 import { CardRowComponent } from './card-row.component';
+import { SemicircleChartComponent } from '../../atoms/semicircle-chart/semicircle-chart.component';
+import { NgApexchartsModule } from "ng-apexcharts";
+import { CardWithCenterTextComponent } from '../../atoms/card/card-with-center-text/card-with-center-text.component';
+import { SemicircleChartCardComponent } from '../../atoms/card/semicircle-chart-card/semicircle-chart-card.component';
 import { CardWithDonutChartComponent } from '../../atoms/card/card-with-donut-chart/card-with-donut-chart.component';
 import * as PlotlyJS from 'plotly.js/dist/plotly.js';
 import { PlotlyModule } from 'angular-plotly.js';
@@ -13,8 +17,14 @@ export default {
   title: 'Molecules/Card Row',
   decorators: [
     moduleMetadata({
-      declarations: [CardRowComponent, CardWithDonutChartComponent],
-      imports: [CommonModule, PlotlyModule],
+      declarations: [
+        CardRowComponent, 
+        CardWithDonutChartComponent, 
+        SemicircleChartCardComponent,
+        SemicircleChartComponent,
+        CardWithCenterTextComponent
+      ],
+      imports: [CommonModule, PlotlyModule, NgApexchartsModule],
     }),
   ],
   argTypes: props,
