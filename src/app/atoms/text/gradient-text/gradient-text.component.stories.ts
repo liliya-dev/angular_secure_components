9eb7844e1e4626bf7eda6cfd1d698f8cc2b4c25a
lili4ka <lili4ka@MacBook-Pro-lili4ka.local>
2020-11-04 13:45:43 +0200

    added ability to add text to round icons

---
+++ src/app/atoms/text/gradient-text/gradient-text.component.stories.ts
@@ -0,0 +1,23 @@
+import { Meta, Story } from '@storybook/angular';
+import { GradientTextComponent } from './gradient-text.component';
+
+const props = {
+  title: { control: 'text' },
+};
+
+export default {
+  title: 'Atoms/Text/Gradient text',
+  component: GradientTextComponent,
+  argTypes: props,
+} as Meta;
+
+const Template: Story<GradientTextComponent> = (args: GradientTextComponent) => ({
+  component: GradientTextComponent,
+  props: args,
+});
+
+export const Green = Template.bind({});
+Green.args = {
+  title: "Send scan reports via email",
+  classes: "fs-16-gramm-300"
+};
