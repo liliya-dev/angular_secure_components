35fd11d8aa3a3db2ee4d07dd378008b6e5bafe42
lili4ka <lili4ka@MacBook-Pro-lili4ka.local>
2020-11-06 17:10:31 +0200

    scan report status card

---
+++ src/app/atoms/card/card-report-status/card-report-status.component.stories.ts
@@ -0,0 +1,31 @@
+import { Meta, Story } from '@storybook/angular';
+import { CardReportStatusComponent } from './card-report-status.component';
+
+const props = {
+  title: { control: 'text' },
+  value: { control: 'text' },
+  text: { control: 'text' },
+  iconSrc: { control: 'text' }
+};
+
+export default {
+  title: 'Atoms/Card',
+  component: CardReportStatusComponent,
+  argTypes: props,
+} as Meta;
+
+const Template: Story<CardReportStatusComponent> = (args: CardReportStatusComponent) => ({
+  component: CardReportStatusComponent,
+  props: args,
+});
+
+export const ReportStatus = Template.bind({});
+ReportStatus.args = {
+  title: "12 aug 2020",
+  texts: [
+    { text: 'totlal', value: '25%' },
+    { text: 'consent', value: '2629' },
+    { text: 'opt-in', value: '1629' },
+    { text: 'opt-out', value: '1000' },
+  ]
+};
