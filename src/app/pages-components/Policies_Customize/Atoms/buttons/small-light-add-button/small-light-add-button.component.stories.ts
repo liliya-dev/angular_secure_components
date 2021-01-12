0d16feea745371cd80171d4b9ce3e9b247620b59
lili4ka <lili4ka@MacBook-Pro-lili4ka.local>
2021-01-12 12:15:08 +0200

    removed global styles from domains and data reuest form

--- src/app/pages-components/Policies_Customize/Atoms/buttons/small-light-add-button/small-light-add-button.component.stories.ts
+++ src/app/pages-components/Policies_Customize/Atoms/buttons/small-light-add-button/small-light-add-button.component.stories.ts
@@ -17,13 +17,13 @@ const Template: Story<PoliciesSmallLightAddButtonComponent> = (args: PoliciesSma
 export const SmallLight = Template.bind({});
 SmallLight.args = {
   title: "Add table",
-  classes: "app-small-light-add-button fs-14-gramm-400-light"
+  classes: "app-small-light-add-button"
 };
 
 export const BigLight = Template.bind({});
 BigLight.args = {
   title: "Add row",
-  classes: "app-big-light-add-button fs-14-gramm-400-light"
+  classes: "app-big-light-add-button"
 };
 
 
