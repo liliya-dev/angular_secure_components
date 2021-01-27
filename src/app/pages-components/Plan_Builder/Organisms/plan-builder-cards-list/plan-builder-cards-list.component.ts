275af05effd408d9460886fb8902ca95c970c04f
lili4ka <lili4ka@MacBook-Pro-lili4ka.local>
2021-01-27 12:38:49 +0200

    plan builder cards list added

---
+++ src/app/pages-components/Plan_Builder/Organisms/plan-builder-cards-list/plan-builder-cards-list.component.ts
@@ -0,0 +1,57 @@
+import { AfterViewInit, Component, HostListener, Input, OnInit, ViewChild } from '@angular/core';
+
+const MOBILE_VIEW = 798;
+const CARD_MOBILE_VIEW = 415;
+
+interface Card {
+  id: number,
+  policyTitle: string,
+  policyText: string,
+  text: string,
+  price: number,
+  imagePath: string
+}
+
+@Component({
+  selector: 'app-plan-builder-cards-list',
+  templateUrl: './plan-builder-cards-list.component.html',
+  styleUrls: ['./plan-builder-cards-list.component.scss']
+})
+
+export class PlanBuilderCardsListComponent implements OnInit, AfterViewInit {
+  @Input() currency: string;
+  @Input() selectedCards: number[];
+  @Input() period: string;
+  @Input() cards: Card[];
+
+  isMobileForContainer: boolean;
+  isMobileForCard: boolean;
+
+  addToSelected(id: number) {
+    const isSelected = this.selectedCards.includes(id);
+    if (isSelected) {
+      this.selectedCards = this.selectedCards.filter(card => card !== id);
+    } else {
+      this.selectedCards = [...this.selectedCards, id]
+    }
+  }
+
+  @ViewChild('container') container;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.isMobileForContainer = this.container.nativeElement.clientWidth < MOBILE_VIEW;
+    this.isMobileForCard = this.container.nativeElement.clientWidth < CARD_MOBILE_VIEW;
+  }
+
+  detectIfSelected(id: number) {
+    return this.selectedCards.includes(id);
+  }
+
+  ngAfterViewInit() {
+    this.isMobileForContainer = this.container.nativeElement.clientWidth < MOBILE_VIEW;
+    this.isMobileForCard = this.container.nativeElement.clientWidth < CARD_MOBILE_VIEW;
+  }
+
+  ngOnInit(): void {}
+}
